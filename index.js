ed664b3b16574d1a4201ee0172d41bbfdcfb40b9
Amir Harrouche <orim.ouf@gmail.com>
2025-04-03 21:37:44 +0100

    msg test

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.post('/sendWhatsapp', function (req, res) {
         to: 'whatsapp:+213791602498'
     })
     .then(
-        res.header("Access-Control-Allow-Origin", "https://www.hermes.com"), // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Origin", "https://www.hermes.com/*"), // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
         res.send('message sended.')
     );
